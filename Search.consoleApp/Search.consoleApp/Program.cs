7c08aba70ddc792af5434e1ac7c822e34e19b14c
mogyiii <50268208+mogyiii@users.noreply.github.com>
2022-09-04 11:57:44 +0200

    Add files via upload

---
+++ Search.consoleApp/Search.consoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    public static void Main()
+    {
+        string searchText = "^b{1}.{4}9504@freemail.hu.{1,}";
+        Regex rgx = new Regex(searchText);
+        //string folderPath = System.Reflection.Assembly.GetEntryAssembly().Location;
+        string folderPath = "D:\\Downloads\\Jelszavak\\";
+        foreach (string file in Directory.GetFiles(folderPath, "*.txt", SearchOption.AllDirectories))
+        {
+            foreach (var line in File.ReadAllLines(file))
+            {
+                if (rgx.IsMatch(line))
+                {
+                    Console.WriteLine($"{line} {file}");
+                }
+            }
+            
+        }
+    }
+}
